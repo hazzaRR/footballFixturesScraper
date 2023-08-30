ff55225c76ed8c8d1312df5a030839a6f1036d5c
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2023-08-30 09:16:34 +0100

    updated scraper to convert string which contains the date to a date object

--- footballFixtures.py
+++ footballFixtures.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+from datetime import datetime
 
 def get_fixtures_links(team):
     team = team.replace(' ', '-').lower()
@@ -32,13 +33,19 @@ def get_match_details(links):
         game_data = soup.find('div', class_='sdc-site-match-header__body')
 
         teams = game_data.find_all('span', class_='sdc-site-match-header__team-name-block-target')
-        kickoff_date = game_data.find('time', class_='sdc-site-match-header__detail-time').text.split(',')
+        kickoff_datetime = game_data.find('time', class_='sdc-site-match-header__detail-time').text.split(',')
+
+        kickoff_date = kickoff_datetime[1].replace('.', '').strip()
+
+        kickoff_date = kickoff_date.split(" ")
+
+        day = kickoff_date[1].replace("st", "").replace("nd", "").replace("rd", "").replace("th", "")
 
         match = {
             'Home Team': teams[0].text,
             'Away Team': teams[1].text,
-            'Kick Off' : kickoff_date[0],
-            'Date': kickoff_date[1].replace('.', ''),
+            'Kick Off' : kickoff_datetime[0],
+            'Date': datetime.strptime(kickoff_date[0] + " " + day + " " + kickoff_date[2] + " " + kickoff_date[3], "%A %d %B %Y"),
             'Stadium': game_data.find('span', class_='sdc-site-match-header__detail-venue').text.replace('.', ''),
             'Competition': game_data.find('p', class_='sdc-site-match-header__detail-fixture').text.split('.')[1]
         }  
@@ -51,7 +58,7 @@ def get_match_details(links):
 
 def save_to_csv(matches, team):
     matchesdf = pd.DataFrame(matches)
-    matchesdf.to_csv(team.replace(' ', '-').lower() + '-fixtures-23-24.csv', index=False)
+    matchesdf.to_csv(team.replace(' ', '-').lower() + '-fixtures-upcoming.csv', index=False)
     print(matchesdf)
 
 
