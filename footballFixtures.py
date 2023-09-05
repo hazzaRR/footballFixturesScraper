1a0c42ec7c4d1722b4e25ae1416329665bf649b0
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2023-09-05 14:28:05 +0100

    created function that connects to postgres database and prints out all the fixtures and matches stored in the database

--- footballFixtures.py
+++ footballFixtures.py
@@ -2,71 +2,113 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 from datetime import datetime
+import psycopg2
+from credentials import DATABASE_PASSWORD
+import os
 
 
-DATE_FORMAT_STRING = "%I:%M%p, %A %d %B %Y"
 
-def get_fixtures_links(team):
-    team = team.replace(' ', '-').lower()
-    url = f'https://www.skysports.com/{team}-fixtures'
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-
-    fixturesLinks = []
-    fixtures = soup.find_all('div', class_='fixres__item')
-
-    for fixture in fixtures:
-
-        matchLink = fixture.find('a', class_="matches__item matches__link")
-        fixturesLinks.append(matchLink['href'])
-
-    return fixturesLinks
+class ScrapeFixtures:
 
 
-def get_match_details(links):
+    DATE_FORMAT_STRING = "%I:%M%p, %A %d %B %Y"
 
-    matches = []
+    def __init__(self):
+        pass
 
-
-    for link in links:
-
-        r = requests.get(link)
+    def get_fixtures_links(self, team):
+        team = team.replace(' ', '-').lower()
+        url = f'https://www.skysports.com/{team}-fixtures'
+        r = requests.get(url)
         soup = BeautifulSoup(r.text, 'html.parser')
 
-        game_data = soup.find('div', class_='sdc-site-match-header__body')
+        fixturesLinks = []
+        fixtures = soup.find_all('div', class_='fixres__item')
 
-        teams = game_data.find_all('span', class_='sdc-site-match-header__team-name-block-target')
-        kickoff_datetime = game_data.find('time', class_='sdc-site-match-header__detail-time').text.split(',')
+        for fixture in fixtures:
 
-        kickoff_date = kickoff_datetime[1].replace('.', '').strip()
+            matchLink = fixture.find('a', class_="matches__item matches__link")
+            fixturesLinks.append(matchLink['href'])
 
-        kickoff_date = kickoff_date.split(" ")
-
-        day = kickoff_date[1].replace("st", "").replace("nd", "").replace("rd", "").replace("th", "")
-
-        match = {
-            'Home Team': teams[0].text,
-            'Away Team': teams[1].text,
-            'Kick Off': datetime.strptime(f"{kickoff_datetime[0]}, {kickoff_date[0]} {day} {kickoff_date[2]} {kickoff_date[3]}", DATE_FORMAT_STRING),
-            'Stadium': game_data.find('span', class_='sdc-site-match-header__detail-venue').text.replace('.', ''),
-            'Competition': game_data.find('p', class_='sdc-site-match-header__detail-fixture').text.split('.')[1],
-            "SkySportsURL": link
-        }  
-
-        matches.append(match)
+        return fixturesLinks
 
 
-    return matches
+    def get_match_details(self, links):
+
+        matches = []
 
 
-def save_to_csv(matches, team):
-    matchesdf = pd.DataFrame(matches)
-    matchesdf.to_csv(team.replace(' ', '-').lower() + '-fixtures-upcoming.csv', index=False)
-    print(matchesdf)
+        for link in links:
+
+            r = requests.get(link)
+            soup = BeautifulSoup(r.text, 'html.parser')
+
+            game_data = soup.find('div', class_='sdc-site-match-header__body')
+
+            teams = game_data.find_all('span', class_='sdc-site-match-header__team-name-block-target')
+            kickoff_datetime = game_data.find('time', class_='sdc-site-match-header__detail-time').text.split(',')
+
+            kickoff_date = kickoff_datetime[1].replace('.', '').strip()
+
+            kickoff_date = kickoff_date.split(" ")
+
+            day = kickoff_date[1].replace("st", "").replace("nd", "").replace("rd", "").replace("th", "")
+
+            match = {
+                'Home Team': teams[0].text,
+                'Away Team': teams[1].text,
+                'Kick Off': datetime.strptime(f"{kickoff_datetime[0]}, {kickoff_date[0]} {day} {kickoff_date[2]} {kickoff_date[3]}", self.DATE_FORMAT_STRING),
+                'Stadium': game_data.find('span', class_='sdc-site-match-header__detail-venue').text.replace('.', ''),
+                'Competition': game_data.find('p', class_='sdc-site-match-header__detail-fixture').text.split('.')[1],
+                "SkySportsURL": link
+            }  
+
+            matches.append(match)
+
+
+        return matches
+
+
+    def save_to_csv(self, matches, team):
+        matchesdf = pd.DataFrame(matches)
+        matchesdf.to_csv(team.replace(' ', '-').lower() + '-fixtures-upcoming.csv', index=False)
+        print(matchesdf)
+
+    def save_to_PostgresDatabase(self, matches):
+
+        try:
+            #establishing the connection
+            conn = psycopg2.connect(
+            database="derbycounty", user='postgres', password=DATABASE_PASSWORD, host='localhost', port= '5432'
+            )
+
+            cursor = conn.cursor()
+
+            cursor.execute("SELECT * FROM upcoming_fixtures")
+
+            print(cursor.fetchall())
+
+            cursor.execute("SELECT * FROM match_results")
+
+            print(cursor.fetchall())
+
+            cursor.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+                print('Database connection closed.')
 
 
 
-teamToFind = input("Which football team do you want to get fixtures for: ")
-matchesList = get_match_details(get_fixtures_links(teamToFind))
+if __name__ == "__main__":
 
-save_to_csv(matchesList, teamToFind)
+    teamToFind = input("Which football team do you want to get fixtures for: ")
+    webscrapperBot = ScrapeFixtures()
+
+    # matchesList = webscrapperBot.get_match_details(webscrapperBot.get_fixtures_links(teamToFind))
+
+    webscrapperBot.save_to_PostgresDatabase(None)
+
+    # webscrapperBot.save_to_csv(matchesList, teamToFind)
